1766620bc3b4b076dc31bffdc17cb38e6dc410e9
Erika Lima <dev.erikamaylim@gmail.com>
2025-05-13 12:35:08 -0300

    update

--- ep01/hello.sql
+++ ep01/hello.sql
@@ -10,5 +10,5 @@ FROM tb_products AS t1
 
 WHERE (t1.product_category_name = 'bebes'
        AND t1.product_photos_qty > 1)
-OR  (t1.product_category_name = 'perfumaria'
+OR (t1.product_category_name = 'perfumaria'
        AND t1.product_photos_qty > 5)
